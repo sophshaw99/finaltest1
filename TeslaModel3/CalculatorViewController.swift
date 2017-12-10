ec5076047d1f2dde4b15578142ec1b7c1e058445
Sophia <sophshaw99@gmail.com>
2017-12-10 12:11:12 -0800

    second commit

---
+++ TeslaModel3/CalculatorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorViewController.swift
+//  TeslaModel3
+//
+//  Created by Sophia Shaw on 12/8/17.
+//  Copyright © 2017 Soph Shaw. All rights reserved.
+//
+
+import Foundation
---
+++ TeslaModel3/PhotoInfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoInfoViewController.swift
+//  TeslaModel3
+//
+//  Created by Sophia Shaw on 12/7/17.
+//  Copyright © 2017 Soph Shaw. All rights reserved.
+//
+
+import Foundation
